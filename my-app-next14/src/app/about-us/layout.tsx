a218735a50e6c86774a285b79a8e2e188d927472
kim jh <odineyes2@gmail.com>
2024-09-04 18:22:07 +0900

    upload code

---
+++ my-app-next14/src/app/about-us/layout.tsx
@@ -0,0 +1,14 @@
+import Navigation from "../../components/navigation";
+
+export default function AboutUsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {children}
+      hahah!!
+    </div>
+  );
+}
---
+++ my-app-next14/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Navigation from "../components/navigation";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 | Not Found :(</h1>
+    </div>
+  );
+}
