c2f6bdd70d85a6ba90e58e863a2561ee509ffda2
odineyes2 <55679108+odineyes2@users.noreply.github.com>
2024-09-05 09:40:59 +0900

    240905 css module (2)

--- my-app-next14/src/components/navigation.tsx
+++ my-app-next14/src/components/navigation.tsx
@@ -2,18 +2,19 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
         <li>
-          <Link href="/">Home</Link> {path === "/" ? "💖" : ""}
+          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
         </li>
         <li>
           <Link href="/about-us">About Us</Link>
-          {path === "/about-us" ? "💖" : ""}
+          {path === "/about-us" ? "🔥" : ""}
         </li>
       </ul>
     </nav>
